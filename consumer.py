ddebc972ff18b80bef76affd77e1c7d34855755f
MertAli07 <123542611+MertAli07@users.noreply.github.com>
2023-09-07 16:15:39 +0300

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,19 @@
+from airflow import DAG, Dataset
+from airflow.decorators import task
+
+from datetime import datetime
+
+my_file = Dataset("/tmp/my_file.txt")
+
+with DAG(
+    dag_id = "consumer",
+    shedule = [my_file],
+    start_date = datetime(2023, 9, 7),
+    catchup = False
+):
+    @task
+    def read_dataset():
+        with open(my_file.uri, "r") as f:
+            print(f.read())
+
+    read_dataset()
