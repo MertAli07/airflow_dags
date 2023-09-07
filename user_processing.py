4d039862f4456fdc8eeb4fa71bdfb6d9d8158643
MertAli07 <123542611+MertAli07@users.noreply.github.com>
2023-09-07 11:40:29 +0300

    Add files via upload

--- user_processing.py
+++ user_processing.py
@@ -1,8 +1,25 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
 
+import json
+from pandas import json_normalize
 from datetime import datetime
+
+def _process_user(ti):
+    user = ti.xcom_pull(task_ids = "extract_user")
+    user = user["results"][0]
+    processed_user = json_normalize({
+        "firstname": user["name"]["first"],
+        "lastname": user["name"]["last"],
+        "country": user["location"]["country"],
+        "username": user["login"]["username"],
+        "password": user["login"]["password"],
+        "email": user["email"]
+    })
+    processed_user.to_csv("/tmp/processed_user.csv", index=None, header=False)
 
 with DAG('user_processing', start_date=datetime(2022, 9, 5), 
         schedule_interval='@daily', catchup=False) as dag:
@@ -22,6 +39,13 @@ with DAG('user_processing', start_date=datetime(2022, 9, 5),
         """
     )
 
+    is_api_available = HttpSensor(
+        task_id = "is_api_available",
+        http_conn_id = "user_api",
+        endpoint = "api/",
+
+    )
+
     extract_user = SimpleHttpOperator(
         task_id = "extract_user",
         http_conn_id = "user_api",
@@ -30,3 +54,10 @@ with DAG('user_processing', start_date=datetime(2022, 9, 5),
         response_filter = lambda response: json.loads(response.text),
         log_response = True
     )
+
+    process_user = PythonOperator(
+        task_id = "extract_user",
+        python_callable = _process_user
+    )
+
+    extract_user >> process_user
